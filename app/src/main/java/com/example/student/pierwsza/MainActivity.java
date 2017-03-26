dabb9250bc8d9575dda849f3d37222006ff41bb7
Rafał Maksim <rafalx117@gmail.com>
2017-03-26 14:06:02 +0200

    Some new comments

--- app/src/main/java/com/example/student/pierwsza/MainActivity.java
+++ app/src/main/java/com/example/student/pierwsza/MainActivity.java
@@ -32,11 +32,14 @@ public class MainActivity extends AppCompatActivity
         final EditText gradesEditText = (EditText) findViewById(R.id.gradesEditText);
 
         //------------------------ odbieramy dane z aktywności DisplayMessage ---------------------------
-        if(User.getName()!=null && User.getName()!=null && User.getGradesCount()!=-1) //jeśli użytkownik zostal zapisany w aktywnosci DisplayMeessage - zostaje on wyświetlony; null, null i -1 to startowe wartosci klasy User i oznaczają, że jeszcze nie został wpisany żaden użytkownik
+        if (User.getName() != null && User.getName() != null && User.getGradesCount() != -1) //jeśli użytkownik zostal zapisany w aktywnosci DisplayMeessage - zostaje on wyświetlony; null, null i -1 to startowe wartosci klasy User i oznaczają, że jeszcze nie został wpisany żaden użytkownik
         {
             nameEditText.setText(User.getName().toString());
             surnameEditText.setText(User.getSurname().toString());
             gradesEditText.setText(String.valueOf(User.getGradesCount()));
+        } else
+        {
+            Toast toast = Toast.makeText(this, "Odczytywanie danych nie powiodło się", Toast.LENGTH_SHORT);
         }
 
         try //próbujemy odczytać średnią z aktywności DisplayMessageActivity
@@ -71,6 +74,91 @@ public class MainActivity extends AppCompatActivity
 
 
 
+
+        TextWatcher textWatcher = new TextWatcher() //tworzymy prosty TextWatcher, który będzie sprawdzał podczas wprowadzaniua tekstu, czy należy pokazac przycisk
+        {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+                setButtonVisibility();
+            }
+
+            // -------- poniższe metody są nieużywane, ale musialy zostać wprowadzone ze względu na TextWatcher -----------------
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+            }
+            //-------------------------------------------------------------------------------------------------------------------
+        };
+
+        nameEditText.addTextChangedListener(textWatcher); /// dodaję TextWatchery do pól name i surname, żeby można było ewentualnie wyświetlić przycisk już przy wprowadzaniu tekstu, a nie dopiero po zmianie focusu
+        surnameEditText.addTextChangedListener(textWatcher);
+
+        gradesEditText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                int grade = 0;
+
+                try //próbujemy wyłuskać ilość ocen z pola gradesEditText
+                {
+                    grade = Integer.parseInt(gradesEditText.getText().toString());
+                } catch (NumberFormatException ex) //w przypadku niepowodzenia - wyświetlamy stosowny komunikat
+                {
+                    numberErrorToast();
+                }
+
+                if (grade < 5 || grade > 15)
+                {
+                    numberErrorToast();
+                    wrongNumber = true;
+                } else
+                {
+                    wrongNumber = false;
+                }
+
+                setButtonVisibility();
+            }
+
+            // -------- poniższe metody są nieużywane, ale musialy zostać wprowadzone ze względu na TextWatcher -----------------
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+            }
+            //-------------------------------------------------------------------------------------------------------------------
+
+        });
+
+
+        gradesEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() //ustawiamy listener na zmianę focusu dla gradesEditText
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (!hasFocus)
+                {
+                    if (gradesEditText.getText().toString().isEmpty()) //jeśli po opuszczeniu pola jest ono puste - wyświetlamy stosowny komunikat
+                    {
+                        validationEmptyErrorToast();
+                    }
+
+                }
+                setButtonVisibility();
+            }
+        });
+
+        //analogicznie ustawiam listener na zmiane focusu dla nameEditText
         nameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener()
         {
             @Override
@@ -87,7 +175,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-
+        //analogicznie ustawiam listener na zmiane focusu dla surnameEditText
         surnameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener()
         {
             @Override
@@ -104,116 +192,23 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        TextWatcher textWatcher = new TextWatcher()
-        {
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
-            {
-                setButtonVisibility();
-            }
-
-            // -------- poniższe metody są nieużywane, ale musialy zostać wprowadzone ze względu na TextWatcher -----------------
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2){}
-            @Override
-            public void afterTextChanged(Editable editable){}
-            //-------------------------------------------------------------------------------------------------------------------
-        };
-
-        nameEditText.addTextChangedListener(textWatcher); /// dodaję TextWatchery do pól name i surname, żeby można było ewentualnie wyświetlić przycisk już przy wprowadzaniu tekstu, a nie dopiero po zmianie focusu
-        surnameEditText.addTextChangedListener(textWatcher);
-
-        gradesEditText.addTextChangedListener(new TextWatcher()
-        {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count)
-            {
-                int grade = 0;
-
-//                if (gradesEditText.getText().toString().isEmpty()) //jeśli pole gradesEditText jest puste - wyświetlamy stosowny komunikat
-//                {
-//                    validationEmptyErrorToast();
-//                }
-//                else
-//                {
-                    try //próbujemy wyłuskać ilość ocen z pola gradesEditText
-                    {
-                        grade = Integer.parseInt(gradesEditText.getText().toString());
-                    }
-                    catch (NumberFormatException ex)
-                    {
-                        numberErrorToast();
-                    }
-                //}
-
-                if (grade < 5 || grade > 15)
-                {
-                    numberErrorToast();
-                    wrongNumber = true;
-                }
-                else
-                {
-                    wrongNumber = false;
-                }
-
-                setButtonVisibility();
-            }
-
-            // -------- poniższe metody są nieużywane, ale musialy zostać wprowadzone ze względu na TextWatcher -----------------
-            @Override
-            public void afterTextChanged(Editable s){}
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2){}
-            //-------------------------------------------------------------------------------------------------------------------
-
-        });
-
-
-        gradesEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-
-                if(!hasFocus){
-                    if(gradesEditText.getText().toString().isEmpty()) {
-                        validationEmptyErrorToast();
-                     }
-
-                }
-            setButtonVisibility();
-            }
-        });
-
-
-//
-//        button = (Button)findViewById(R.id.button);
-//        button.setOnClickListener(
-//                new View.OnClickListener(){
-//                    public void onClick(View v){
-////                        Intent intent = new Intent(this, DisplayMessageActivity.class);
-////                        startActivity(intent);
-//                    }
-//                }
-//        );
-
-
     }
 
-    public void sendMessage(View view)
+    public void sendMessage(View view) //metoda wysyłająca dane do drugiej aktywności
     {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
-        EditText surnameEditText = (EditText) findViewById(R.id.surnameEditText) ;
-        EditText gradesEditText = (EditText) findViewById(R.id.gradesEditText) ;
+        EditText surnameEditText = (EditText) findViewById(R.id.surnameEditText);
+        EditText gradesEditText = (EditText) findViewById(R.id.gradesEditText);
 
         gradesCount = Integer.parseInt(gradesEditText.getText().toString());
-        User.setUser(nameEditText.getText().toString(), surnameEditText.getText().toString(), gradesCount); //zapisujemy użytkownika
+        User.setUser(nameEditText.getText().toString(), surnameEditText.getText().toString(), gradesCount); //zapisujemy użytkownika w statycznej klasie USer
         intent.putExtra("gradesCount", gradesCount);
         startActivity(intent);
     }
 
 
-    private void setButtonVisibility()
+    private void setButtonVisibility() //metoda sprawdza, czy wszystkie wymagane pola są wypełnione oraz czy dane są ok, a następnie pokazuje lub chowa przycisk
     {
         EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
         EditText surnameEditText = (EditText) findViewById(R.id.surnameEditText);
@@ -221,9 +216,9 @@ public class MainActivity extends AppCompatActivity
 
         if (
                 nameEditText.getText().toString().isEmpty() ||
-                surnameEditText.getText().toString().isEmpty() ||
-                gradesEditText.getText().toString().isEmpty() ||
-                wrongNumber
+                        surnameEditText.getText().toString().isEmpty() ||
+                        gradesEditText.getText().toString().isEmpty() ||
+                        wrongNumber //jeśli liczba ocen nie nalezy do odpowiedniego przedziału to wrongNumber = true
                 )
         {
             mainButton.setVisibility(View.INVISIBLE);
@@ -235,13 +230,13 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    public void validationEmptyErrorToast()
+    public void validationEmptyErrorToast() //metoda wyświetlająca komunikat o niewypełnionym polu
     {
         Toast toast = Toast.makeText(this, "Pole nie może być puste", Toast.LENGTH_SHORT);
         toast.show();
     }
 
-    public void numberErrorToast()
+    public void numberErrorToast() //metoda wyświetlająca komunikat o nieprawidłowym wypełnieniu gradesEditText
     {
         wrongNumber = true;
         Toast toast = Toast.makeText(this, "Liczba musi być z zakresu 5-15", Toast.LENGTH_SHORT);
